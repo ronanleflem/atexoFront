46e4c5f6db417d63edc651b5515ce46a36417104
ronanleflem <ronan.leflem@gmail.com>
2024-07-05 18:35:21 +0200

    add route and HTTP module to have visual HTML page and send request

--- atexo-front/src/app/app-routing.module.ts
+++ atexo-front/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InscrireComponent } from './component/inscrire/inscrire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inscrire', component: InscrireComponent },
+  { path: '', redirectTo: '/inscrire', pathMatch: 'full' }  // Route par défaut
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
